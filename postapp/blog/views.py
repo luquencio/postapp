44710eb4b751987dec14b80ff6b38c34e8dc4f42
lcsexr <lucas.evans.carpi@gmail.com>
2015-10-31 14:50:33 -0400

    route for templates fixed

--- postapp/blog/views.py
+++ postapp/blog/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
+from django.http import HttpResponse
+from django.utils import timezone
+from .models import Post 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+	return render(request, 'blog/post_list.html', {})
